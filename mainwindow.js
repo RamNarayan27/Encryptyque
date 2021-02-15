ed88e3c1a20dc1b366b91c12b1ef9fe74c2eb774
RamNarayan27 <ramnarayanbahadur@gmail.com>
2021-02-16 00:50:46 +0530

    half baked code

--- mainwindow.js
+++ mainwindow.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const https = require("https");
 const Swal = require('sweetalert2')
 const axios = require("axios");
-const cred_to_rem = require('data-store')({ path: process.cwd() + '/creds.json' });
+const cred = require('data-store')({ path: process.cwd() + '/creds.json' });
 const { parse, stringify } = require("flatted");
 const NodeRSA = require("node-rsa");
 
@@ -10,3 +10,12 @@ const NodeRSA = require("node-rsa");
 const server_public_key =
   "\n-----BEGIN PUBLIC KEY-----\nMFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAI/Ip/FSDW2ZQfUSfbrFJrVx95crrvUg\n5pi8GEZ5Z1Ahw3UwQlcqQqPlC0FKDcWSvDk1Md7wpk5/PpkxVH6AAK0CAwEAAQ==\n-----END PUBLIC KEY-----\n";
 const server_pubkey = NodeRSA(server_public_key, "pkcs8-public-pem");
+
+const profilePicture = document.getElementById("profile-picture");
+const userFullName = document.getElementById("user-fn");
+const userId = document.getElementById("user-id");
+const quotes = document.getElementById("quotes");
+
+function setStartUp(){
+
+}
