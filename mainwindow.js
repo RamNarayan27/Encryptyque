aaee61c3da2573619a878cc3d023ba261af6a312
Vishal Vignesh <vishalvignesh.15@gmail.com>
2021-02-16 05:43:59 +0530

    Completed inbox frontend+backend
    -Completed frontend+backend connection
    -Added alerts
    -Report now makes use of separate table

--- mainwindow.js
+++ mainwindow.js
@@ -6,6 +6,7 @@ const Base64 = require("js-base64");
 const cred = require("data-store")({ path: process.cwd() + "/creds.json" });
 conf_name = cred.get("unique-username");
 const crypto = require("crypto");
+const shell = require("electron").shell;
 const conf = require("data-store")({
   path: process.cwd() + "/" + conf_name + ".json",
 });
@@ -79,55 +80,88 @@ async function delete_file(downloadID) {
   let a_fname = inbox.get(downloadID);
   a_fname = a_fname["actual_name"];
   const path = process.cwd() + "\\inbox\\" + a_fname;
-
-  try {
-    fs.access(path, fs.F_OK, (err) => {
-      if (err) {
-        console.error(err);
-        return;
+  Swal.fire({
+    title: "Delete File?",
+    html:
+      '<p style="color:#FFF";>Are you sure you wish to delete this file? This file will not be stored by Encryptyque anymore</p>',
+    icon: "info",
+    background: "#000000",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Delete",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      inbox.del(downloadID);
+      try {
+        fs.access(path, fs.F_OK, (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          fs.unlink(
+            path,
+            (callback = function () {
+              console.log("Deleted file");
+            })
+          );
+        });
+      } catch (err) {
+        console.log("File hasnt been Downloaded");
       }
-      fs.unlink(
-        path,
-        (callback = function () {
-          console.log("Deleted file");
-        })
-      );
-    });
-  } catch (err) {
-    console.log("File hasnt been Downloaded");
-  }
+      const docClient = new AWS.DynamoDB.DocumentClient();
+      const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+      const user_table = "transaction-manifest";
+      var bucket_params = {
+        Key: downloadID,
+        Bucket: "cnproject-bucket",
+      };
+      const table_params = {
+        TableName: user_table,
+        Key: {
+          transactionid: downloadID,
+        },
+      };
 
-  inbox.del(downloadID);
-  const docClient = new AWS.DynamoDB.DocumentClient();
-  const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
-  const user_table = "transaction-manifest";
-  var bucket_params = {
-    Key: downloadID,
-    Bucket: "cnproject-bucket",
-  };
-  const table_params = {
-    TableName: user_table,
-    Key: {
-      transactionid: downloadID,
-    },
-  };
+      docClient.delete(table_params, function (err, data) {
+        if (err) {
+          console.error("Failed to Delete Share");
+        } else {
+          console.log("Successfully Deleted Share");
+        }
+      });
 
-  docClient.delete(table_params, function (err, data) {
-    if (err) {
-      console.error("Failed to Delete Share");
-    } else {
-      console.log("Successfully Deleted Share");
+      s3.deleteObject(bucket_params, function (err, data) {
+        if (err) console.log("Failed to Delete File");
+        else console.log("Deleted File on Storage");
+      });
+      Swal.fire({
+        title: "Success",
+        html:
+          '<p style="color:#FFF";>The file has been deleted successfully.</p>',
+        icon: "success",
+        background: "#000000",
+        showCancelButton: false,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Delete",
+      })
+      generateCards();
     }
   });
-
-  s3.deleteObject(bucket_params, function (err, data) {
-    if (err) console.log("Failed to Delete File");
-    else console.log("Deleted File on Storage");
-  });
-  generateCards()
+  generateCards();
 }
 
 async function download_file(downloadID) {
+  Swal.fire({
+    icon: "info",
+    title: '<p style="color:#FFF";>Please Wait</p>',
+    width: "350",
+    html: '<p style="color:#FFF";>Please wait while the file downloads</p>',
+    background: "#000000",
+    allowOutsideClick: false,
+    showConfirmButton: false,
+  });
   const priv_key = new NodeRSA(myprivatekey(), "pkcs1-private-pem");
   const ef_info = inbox.get(downloadID);
   const ef_name = ef_info.filename;
@@ -167,18 +201,74 @@ async function download_file(downloadID) {
       for (var i = 0; i < length; i++) {
         ua[i] = binaryImg.charCodeAt(i);
       }
-      fs.writeFileSync(
-        process.cwd() + "/inbox/" + ef_actual_name,
-        ua
-      );
+      fs.writeFileSync(process.cwd() + "/inbox/" + ef_actual_name, ua);
+      Swal.fire({
+        icon: "success",
+        title: '<p style="color:#FFF";>Downloaded Successfully</p>',
+        width: "350",
+        html:
+          '<p style="color:#FFF";>The File has downloaded successfully. Check your inbox folder</p>',
+        background: "#000000",
+        allowOutsideClick: false,
+        showConfirmButton: true,
+      });
+      generateCards();
     }
   });
+  generateCards();
 }
 
 async function report_file(data) {
-  require("shell").openExternal(
-    `mailto:helpdesk.encryptyque@gmail.com?subject=Malicious File&body=${data}`
-  );
+  tempdata = inbox.get(data);
+  Swal.fire({
+    icon: "info",
+    title: '<p style="color:#FFF";>Report File?</p>',
+    html:
+      '<p style="color:#FFF";>Are you sure you wish to report the file? Reporting the file will reveal the contents of the file to the developers</p>',
+    background: "#000000",
+    showConfirmButton: true,
+    showCancelButton: true,
+    allowOutsideClick: true,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const docClient = new AWS.DynamoDB.DocumentClient();
+      const user_table = "report-table";
+      
+      const priv_key = new NodeRSA(myprivatekey(), "pkcs1-private-pem");
+      const ef_info = inbox.get(tempdata['transactionid']);
+      const ef_key = Buffer.from(ef_info.encryptionkey);
+      const ef_iv = Buffer.from(ef_info.encryptionIV);
+      dc_iv = priv_key.decrypt(ef_iv);
+      dc_key = priv_key.decrypt(ef_key);
+      const params = {
+        TableName: user_table,
+        Item: {
+          transactionid: tempdata['transactionid'],
+          username: conf.get("unique-username"),
+          data: stringify(tempdata),
+          dc_key: stringify(dc_key),
+          dc_iv: stringify(dc_iv)
+        },
+      };
+
+      docClient.put(params, function (err, data) {
+        if (err) {
+          console.error("Failed To Report", err);
+        } else {
+          console.log("Successfully Reported");
+          Swal.fire({
+            icon: "success",
+            title: '<p style="color:#FFF";>File Reported!</p>',
+            html:
+              '<p style="color:#FFF";>The file has been successfully reported.</p>',
+            background: "#000000",
+            showConfirmButton: true,
+            allowOutsideClick: true,
+          })
+        }
+      });
+    }
+  });
 }
 
 async function setinboxlength() {
@@ -237,35 +327,46 @@ async function refresh_check() {
         notifier_info("New Shares Arrived");
       }
     }
+    generateCards();
   }
-  generateCards();
 }
 
 function generateCards() {
-    inbox_html.innerHTML = ''
-    const inbox_data = fs.readFileSync("inbox.json");
-    const inbox_i = JSON.parse(inbox_data);
-    const len = Object.keys(inbox_i);
-    
-    console.log(len)
-    for(let dis_key of len){
-      inbox_html.innerHTML = inbox_html.innerHTML + `<div class="column is-one-quarter">
+  inbox_html.innerHTML = "";
+  const inbox_data = fs.readFileSync("inbox.json");
+  const inbox_i = JSON.parse(inbox_data);
+  const len = Object.keys(inbox_i);
+
+  console.log(len);
+  for (let dis_key of len) {
+    inbox_html.innerHTML =
+      inbox_html.innerHTML +
+      `<div class="column is-one-quarter">
       <div class="card">
-          <div class="card-content has-text-centered">From<br>${inbox.get(dis_key).from}<br>File
+          <div class="card-content has-text-centered">From<br>${
+            inbox.get(dis_key).from
+          }<br>File
               Name:<br>${inbox.get(dis_key).actual_name}<br></div>
           <div class="card-footer">
-              <div class="card-footer-item" onclick="download_file('${inbox.get(dis_key).transactionid}')"><span class="icon material-icons">download</span>
+              <div class="card-footer-item" onclick="download_file('${
+                inbox.get(dis_key).transactionid
+              }')"><span class="icon material-icons">download</span>
               </div>
-              <div class="card-footer-item" onclick="delete_file('${inbox.get(dis_key).transactionid}')"><span class="icon material-icons">delete</span>
+              <div class="card-footer-item" onclick="delete_file('${
+                inbox.get(dis_key).transactionid
+              }')"><span class="icon material-icons">delete</span>
               </div>
-              <div class="card-footer-item" onclick="report_file(${inbox.get(dis_key)})"><span class="icon material-icons">report</span>
+              <div class="card-footer-item" onclick="report_file('${
+                inbox.get(dis_key).transactionid
+              }')"><span class="icon material-icons">report</span>
               </div>
           </div>
-          <div class="has-text-centered">${inbox.get(dis_key).ReceivedTime}</div>
+          <div class="has-text-centered">${
+            inbox.get(dis_key).ReceivedTime
+          }</div>
       </div>
   </div>`;
+  }
 }
-}
-    
-setStartUp();
 
+setStartUp();
